4b39e5ea54322864e41550ff23d9031739797615
PushpaLatha Moparthi <pushpalatha.moparthi@apicasystems.com>
2020-12-07 22:31:43 +0100

    Added new tests in API

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,19 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
-import utilities.HTTPMethods;
-
-import java.io.IOException;
+import io.restassured.response.Response;
 import utilities.API;
-import org.json.JSONObject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 
 public class APITests {
 
+    private static Logger logger = LogManager.getLogger();
+
     @BeforeClass
     private void setUp() {
+
         System.out.println("Make sure that ZebraTester is up and running");
+
     }
 
     @AfterClass
@@ -22,48 +29,200 @@ public class APITests {
     }
 
     @Test
-    public void testGetRecorderVersion() {
+    public void GetRecorderVersion_Test()  {
 
-        try {
 
-            String response = HTTPMethods.GETRequestResponse(API.getRecorderVersion());
-            System.out.println("Response Code: " + response.toString());
-
-            JSONObject apiJsonObj = new JSONObject(response);
-
-            String versionValue = HTTPMethods.getValuefromJSON(apiJsonObj, "RecorderVersion");
-            System.out.println("Recorded Version is :" + versionValue);
-
-        } catch (IOException e) {
-
-            System.out.println(e.getMessage());
-            System.out.println(e.getStackTrace().toString());
-        }
+        RequestSpecification request = RestAssured.given();
+        logger.info(API.getRecorderVersion());
+        request.contentType(ContentType.JSON);
+        Response response = request.get(API.getRecorderVersion());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get("RecorderVersion").toString(), API.getinstalledZTVersion_data());
     }
 
-        @Test
-        public void verifyGetRecorderVersion()  {
 
-            try{
+    @Test
+    public void getRecorderSSLVersion_Test() {
 
-                String response = HTTPMethods.GETRequestResponse(API.getRecorderVersion());
-                System.out.println("Response Code: " + response.toString());
-
-                JSONObject apiJsonObj = new JSONObject(response);
-
-                String versionValue = HTTPMethods.getValuefromJSON(apiJsonObj, "RecorderVersion");
-                System.out.println("Recorded Version is :" + versionValue);
-                Assert.assertEquals(versionValue, API.getinstalledVersion());
-
-            }
-            catch(IOException e){
-
-                System.out.println(e.getMessage());
-                System.out.println(e.getStackTrace().toString());
-            }
+        RequestSpecification request = RestAssured.given();
+        logger.info(API.getRecorderSSLVersion());
+        request.contentType(ContentType.JSON);
+        Response response = request.get(API.getRecorderSSLVersion());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get("SSLVersion").toString(),API.getinstalledSSLVersion_data());
 
     }
 
+    @Test
+    public void setRecorderSSLVersion_Test() {
+
+        RequestSpecification request;
+
+        request = RestAssured.given();
+        logger.info(API.setRecorderSSLVersiontls12());
+        request.contentType(ContentType.JSON);
+        Response response = request.post(API.setRecorderSSLVersiontls12());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+
+        logger.info(API.getRecorderSSLVersion());
+        request = RestAssured.given();
+        request.contentType(ContentType.JSON);
+        response = request.get(API.getRecorderSSLVersion());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get("SSLVersion").toString(),"tls12");
+
+        logger.info(API.setRecorderSSLVersionAll());
+        request = RestAssured.given();
+        request.contentType(ContentType.JSON);
+        response = request.post(API.setRecorderSSLVersionAll());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+
+        logger.info(API.getRecorderSSLVersion());
+        request = RestAssured.given();
+        request.contentType(ContentType.JSON);
+        response = request.get(API.getRecorderSSLVersion());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get("SSLVersion").toString(),"all");
+
+
+    }
+
+    @Test
+    public void getRecorderDNSConfig_Test() {
+
+
+        RequestSpecification request = RestAssured.given();
+        logger.info(API.getRecorderDNSConfig());
+        request.contentType(ContentType.JSON);
+        Response response = request.get(API.getRecorderDNSConfig());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get().toString(), "{}");
+
+    }
+
+    @Test
+    public void getRecorderDNSTranslationTable_Test() {
+
+        RequestSpecification request;
+
+        request = RestAssured.given();
+        logger.info(API.setRecorderDNSConfig());
+        request.contentType(ContentType.JSON);
+        request.body(API.DNSconfig_data());
+        Response response = request.get(API.setRecorderDNSConfig());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get().toString(), "{}");
+
+        request = RestAssured.given();
+        logger.info(API.getRecorderDNSTranslationTable());
+        request.contentType(ContentType.JSON);
+        response = request.get(API.getRecorderDNSTranslationTable());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get().toString(), "{}");
+
+    }
+
+
+    @Test
+    public void recorderPluginsGetList_Test() {
+
+        RequestSpecification request = RestAssured.given();
+        logger.info(API.recorderPluginsGetList());
+        request.contentType(ContentType.JSON);
+        Response response = request.get(API.recorderPluginsGetList());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get("RecorderPlugins").toString(), "[]");
+
+    }
+
+
+    @Test
+    public void clearRecorderDNSResolveCache_Test() {
+
+        RequestSpecification request;
+
+        request = RestAssured.given();
+        logger.info(API.setRecorderDNSConfig());
+        request.contentType(ContentType.JSON);
+        request.body(API.DNSconfig_data());
+        Response response = request.get(API.setRecorderDNSConfig());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get().toString(), "{}");
+
+        request = RestAssured.given();
+        logger.info(API.clearRecorderDNSResolveCache());
+        request.contentType(ContentType.JSON);
+        response = request.get(API.clearRecorderDNSResolveCache());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get().toString(), "{}");
+
+        request = RestAssured.given();
+        logger.info(API.setRecorderDNSConfig());
+        request.contentType(ContentType.JSON);
+        request.body("{}");
+        response = request.get(API.setRecorderDNSConfig());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get().toString(), "{}");
+
+
+
+    }
+
+    @Test
+    public void setRecorderDNSConfig_Test() {
+
+        RequestSpecification request;
+
+        request = RestAssured.given();
+        logger.info(API.setRecorderDNSConfig());
+        request.contentType(ContentType.JSON);
+        request.body(API.DNSconfig_data());
+        Response response = request.get(API.setRecorderDNSConfig());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get().toString(), "{}");
+
+
+        logger.info(API.getRecorderDNSConfig());
+        request = RestAssured.given();
+        request.contentType(ContentType.JSON);
+        response = request.get(API.getRecorderDNSConfig());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertTrue(response.jsonPath().get().toString().contains("DNSServers"));
+        Assert.assertEquals(response.jsonPath().get("DNSServers").toString(),"[1.2.3.4, 10.2.28.5]");
+
+
+        request = RestAssured.given();
+        logger.info(API.setRecorderDNSConfig());
+        request.contentType(ContentType.JSON);
+        request.body("{}");
+        response = request.get(API.setRecorderDNSConfig());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get().toString(), "{}");
+
+        logger.info(API.getRecorderDNSConfig());
+        request.contentType(ContentType.JSON);
+        response = request.get(API.getRecorderDNSConfig());
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("JSON Response " + response.jsonPath().get());
+        Assert.assertEquals(response.jsonPath().get().toString(), "{}");
+
+    }
 
 
 }
