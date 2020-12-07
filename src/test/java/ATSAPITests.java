f9f8c550134600b2c3c9f7f97ef169be8a68a569
PushpaLatha Moparthi <pushpalatha.moparthi@apicasystems.com>
2020-12-07 22:12:06 +0100

    Modified imports to fix the compilation errors

--- src/test/java/ATSAPITests.java
+++ src/test/java/ATSAPITests.java
@@ -1,4 +1,4 @@
-import org.json.JSONObject;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -10,11 +10,7 @@ import io.restassured.response.Response;
 import io.restassured.http.ContentType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.hamcrest.Matchers;
 
-import java.util.Random;
 
 
 public class ATSAPITests {
